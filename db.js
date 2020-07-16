6674960d9e98a29e845ab2cb65e9494421a5140e
KateLockhart <katelockhart707@gmail.com>
2020-07-16 00:42:05 -0400

    server

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASS, {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASS, process.env.DATABASE_URL, {
     dialect: 'postgres'
 });
 
