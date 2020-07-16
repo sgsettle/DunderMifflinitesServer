ef10f7aacf1bf86d6092c07fa5fea2b93a61b0e2
KateLockhart <katelockhart707@gmail.com>
2020-07-16 01:22:53 -0400

    last one

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASS, process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 });
 
